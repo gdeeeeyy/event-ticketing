3f93aedeea5d4f0bf8d7109021e5ebb48322a29b
gdeeeeyy <gd14405@gmail.com>
2024-11-23 01:22:32 +0530

    now the mails come with qr codes - have to center the logo in the mail template - setup the doman - map all the users and finally set up the mailing list

--- emails/mail.tsx
+++ emails/mail.tsx
@@ -1,141 +1,141 @@
 import {
-    Body,
-    Container,
-    Column,
-    Head,
-    Html,
-    Img,
-    Link,
-    Preview,
-    Row,
-    Section,
-    Text,
-  } from "@react-email/components";
-  import React, { useState } from "react";
-  // import QRCode from 'react-qr-code';
-  import QRCode from 'qrcode';
-  
-  interface mailDeets {
-    Name?: string,
-    Events?: string,
-    Deets?:{ [key: string]: any };
-  }
-  
-  export const MailTemplate = ({
-    Name, 
-    Events,
-    Deets
-  }: mailDeets) => {
-  const value={Deets};
-    return (
-      <Html>
-        <Head />
-        <Preview>Confirmation of Your Registration for Kalaiyugam on Nov 30, 2024 - Reg.</Preview>
-        <Body style={main}>
-          <Container style={container}>
-            <Section style={logo}>
-            <a href="https://imgur.com/ks7TzIs"><img src="https://i.imgur.com/ks7TzIs.png" title="source: imgur.com" width="100px" /></a>
-            </Section>
-            <Section style={sectionsBorders}>
-              <Row>
-                <Column style={sectionBorder} />
-                <Column style={sectionCenter} />
-                <Column style={sectionBorder} />
-              </Row>
-            </Section>
-            <Section style={content}>
-              <Text style={paragraph}>Greetings {Name}!</Text>
-              <Text style={paragraph}>
-              We are delighted to confirm your registration for Kalaiyugam: A Celebration of Art, our flagship event celebrating creativity, scheduled for 30th November 2024. This email serves as both a confirmation of your participation and an official On-Duty letter, permitting you to attend the event.
-              </Text>
-              <Text style={paragraph}>
-              We trust that you will bring your best self, uphold the highest standards of conduct, and engage responsibly as we welcome you to Kalaiyugam—a celebration you will not forget.
-              </Text>
-              <Text style={paragraph}>
-              Please ensure you carry a copy of this email as it serves as your OD letter and save the attached QR code for verification of your registration for the following events {Events}.
-              </Text>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px' }}>
-              {value && (
-                    <QRCode
-                        value={Deets}
-                        bgColor="#FFFFFF"
-                        fgColor="#000000"
-                        size="35%"
-                    />
-                )}
-                </div>
-              <Text style={paragraph}>
-              If you have any questions or require further assistance, feel free to contact us at{" "}
-                <Link href="impact@citchennai.net" style={link}>
+  Body,
+  Container,
+  Column,
+  Head,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
+import React from "react";
+
+interface mailDeets {
+  Name?: string;
+  Events?: string;
+  Deets?: string;
+}
+
+// The email template (with QR code URL passed as prop)
+export const MailTemplate = ({ Name, Events, Deets }: mailDeets) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>Confirmation of Your Registration for Kalaiyugam on Nov 30, 2024 - Reg.</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={logo}>
+            <div>
+            <a href="https://imgur.com/nQuHLgb">
+              <img
+                src="https://i.imgur.com/nQuHLgb.png"
+                title="source: imgur.com"
+                width="350px"
+              />
+            </a>
+            </div>
+          </Section>
+          <Section style={sectionsBorders}>
+            <Row>
+              <Column style={sectionBorder} />
+              <Column style={sectionCenter} />
+              <Column style={sectionBorder} />
+            </Row>
+          </Section>
+          <Section style={content}>
+            <Text style={paragraph}>Greetings {Name}!</Text>
+            <Text style={paragraph}>
+              We are delighted to confirm your registration for Kalaiyugam: A
+              Celebration of Art, our flagship event celebrating creativity,
+              scheduled for 30th November 2024. This email serves as both a
+              confirmation of your participation and an official On-Duty letter,
+              permitting you to attend the event.
+            </Text>
+            <Text style={paragraph}>
+              We trust that you will bring your best self, uphold the highest
+              standards of conduct, and engage responsibly as we welcome you to
+              Kalaiyugam—a celebration you will not forget.
+            </Text>
+            <Text style={paragraph}>
+              Please ensure you carry a copy of this email as it serves as your
+              OD letter and save the attached QR code for verification of your
+              registration for the following events {Events}.
+            </Text>
+            <div style={{ textAlign: 'center', margin: '20px 0' }}>
+              <Img src={`http://api.qrserver.com/v1/create-qr-code/?data=${Deets}`} width="150" height="150"/>
+            </div>
+            <Text style={paragraph}>
+              If you have any questions or require further assistance, feel free
+              to contact us at{" "}
+              <Link href="mailto:impact@citchennai.net" style={link}>
                 impact@citchennai.net
-                </Link>{" "}
-              </Text>
-              <Text style={paragraph}>
-                With Regards,
-                <br />
-                Team Impact
-              </Text>
-            </Section>
-          </Container>
-              {/* <img>{QrCode}</img> */}
-        </Body>
-      </Html>
-    );
-  };
-  
-  
-  export default MailTemplate;
-  
-  const fontFamily = "HelveticaNeue,Helvetica,Arial,sans-serif";
-  
-  const main = {
-    backgroundColor: "#efeef1",
-    fontFamily,
-  };
-  
-  const paragraph = {
-    lineHeight: 1.5,
-    fontSize: 14,
-  };
-  
-  const container = {
-    maxWidth: "580px",
-    margin: "30px auto",
-    backgroundColor: "#ffffff",
-  };
-  
-  const footer = {
-    maxWidth: "580px",
-    margin: "0 auto",
-  };
-  
-  const content = {
-    padding: "5px 20px 10px 20px",
-  };
-  
-  const logo = {
-    display: "flex",
-    justifyContent: "center",
-    alingItems: "center",
-    padding: 30,
-  };
-  
-  const sectionsBorders = {
-    width: "100%",
-    display: "flex",
-  };
-  
-  const sectionBorder = {
-    borderBottom: "1px solid  rgb(0, 0, 0)",
-    width: "249px",
-  };
-  
-  const sectionCenter = {
-    borderBottom: "1px solid  rgb(0, 0, 0)",
-    width: "102px",
-  };
-  
-  const link = {
-    textDecoration: "underline",
-  };
-  
+              </Link>
+            </Text>
+            <Text style={paragraph}>
+              With Regards,
+              <br />
+              Team Impact
+            </Text>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default MailTemplate;
+
+// CSS for the email content
+const fontFamily = "HelveticaNeue,Helvetica,Arial,sans-serif";
+const main = {
+  backgroundColor: "#efeef1",
+  fontFamily,
+};
+
+const paragraph = {
+  lineHeight: 1.5,
+  fontSize: 14,
+};
+
+const container = {
+  maxWidth: "580px",
+  margin: "30px auto",
+  backgroundColor: "#ffffff",
+};
+
+const footer = {
+  maxWidth: "580px",
+  margin: "0 auto",
+};
+
+const content = {
+  padding: "5px 20px 10px 20px",
+};
+
+const logo = {
+  display: "grid",
+  placeItems: "center",
+  padding: 30,
+};
+
+const sectionsBorders = {
+  width: "100%",
+  display: "flex",
+};
+
+const sectionBorder = {
+  borderBottom: "4px solid rgb(0, 0, 0)",
+  width: "249px",
+};
+
+const sectionCenter = {
+  borderBottom: "4px solid rgb(0, 0, 0)",
+  width: "102px",
+};
+
+const link = {
+  textDecoration: "underline",
+};
